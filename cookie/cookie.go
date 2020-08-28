85c7cf121d6f3f5eeae8d90624b4e0f4d266aaf8
ssoyyoung <ssoyyoung.p@gmail.com>
2020-08-28 14:49:00 +0900

    read cookies

--- cookie/cookie.go
+++ cookie/cookie.go
@@ -1,14 +1,15 @@
 package cookie
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/labstack/echo"
 )
 
-// WriteCookie func
-func WriteCookie(c echo.Context) error {
+// WriteCookies func
+func WriteCookies(c echo.Context) error {
 	cookie := new(http.Cookie)
 	cookie.Name = "uid"
 	cookie.Value = "test"
@@ -16,4 +17,18 @@ func WriteCookie(c echo.Context) error {
 	c.SetCookie(cookie)
 
 	return c.String(http.StatusOK, "write a cookie")
+}
+
+// ReadCookies func
+func ReadCookies(c echo.Context) error {
+	cookie, err := c.Cookie("uid")
+	if err != nil {
+		return err
+	}
+	fmt.Println(cookie)
+	fmt.Println(cookie.Name)
+	fmt.Println(cookie.Value)
+
+	return c.String(http.StatusOK, "read a cookie")
+
 }
